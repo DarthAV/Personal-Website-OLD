f10171568f8fcb03afbd1bc5edbc7da51df0b987
Ansh <Ansh.Verma22@outlook.com>
2021-12-09 00:50:24 -0800

    Added assets (#1)
    Added all assets that are currently needed including Images, Resume, Fonts, and Icons.
    Slightly restructured webpage and adjusted styling for release.
    
    * Add resume section
    
    * Upload profile pictures
    
    * Added socials
    
    * Gotham Font Upload
    
    * Add page favicon
    
    * Compressed images to reduce page load time
    
    * Add experience section, renamed from qualification

--- assets/script.js
+++ assets/script.js
@@ -43,7 +43,7 @@ skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 
-/*============== Qualification Skills ===============*/
+/*============== Experience Skills ===============*/
 
 /*const tabs = document.querySelectorAll('[data-target]'),
       tabContents = document.querySelectorAll('[data-content]')
@@ -51,13 +51,13 @@ tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.target)
         tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('qualification__active')
+            tabContent.classList.remove('experience__active')
         })
-        target.classList.add('qualification__active')
+        target.classList.add('experience__active')
         tab.forEach(tab =>{
-            tab.classList.remove('qualification__active')
+            tab.classList.remove('experience__active')
         })
-        tab.classList.add('qualification__active')
+        tab.classList.add('experience__active')
     })
 })      
 */
