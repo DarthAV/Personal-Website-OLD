96f5991e73ec2316f30cb40c64f1d552b11fc9de
Ansh Verma <ansh.verma22@outlook.com>
2022-05-11 20:39:11 -0700

    fix theme button bug. Clear localstorage needed?

--- assets/script.js
+++ assets/script.js
@@ -148,7 +148,7 @@ const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'icon-ic_fluent_weather_moon_24_regular' : 'icon-ic_fluent_weather_sunny_24_regular'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'icon-ic_fluent_weather_sunny_24_regular' : 'icon-ic_fluent_weather_moon_24_regular'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
